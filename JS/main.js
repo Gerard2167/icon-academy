081b00ceceb2b450ca185381b28f52d6c95184e9
Gerardo <gerardm.2410@gmail.com>
2026-06-09 00:58:19 -0500

    Actualización en el Hero y el Opening

--- JS/main.js
+++ JS/main.js
@@ -243,3 +243,56 @@ setInterval(() => {
     },1000);
 
 },5000);
+
+
+/* =====================================
+   CONTADOR DE PROMO
+===================================== */
+
+const targetDate =
+new Date("June 16, 2026 23:59:59").getTime();
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    const distance =
+    targetDate - now;
+
+    const days =
+    Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours =
+    Math.floor(
+        (distance % (1000 * 60 * 60 * 24))
+        /
+        (1000 * 60 * 60)
+    );
+
+    const minutes =
+    Math.floor(
+        (distance % (1000 * 60 * 60))
+        /
+        (1000 * 60)
+    );
+
+    const seconds =
+    Math.floor(
+        (distance % (1000 * 60))
+        /
+        1000
+    );
+
+    document.getElementById("days").innerText =
+    days;
+
+    document.getElementById("hours").innerText =
+    hours;
+
+    document.getElementById("minutes").innerText =
+    minutes;
+
+    document.getElementById("seconds").innerText =
+    seconds;
+
+},1000);
